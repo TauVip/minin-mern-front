d72268c83b9d861b1056e50106a9615d90717929
TauVip <Abylai1393@gmail.com>
2020-10-18 16:03:05 +0600

    commit 2

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -5,17 +5,31 @@ export const useHttp = () => {
   const [error, setError] = useState(null)
 
   const request = useCallback(async(url, method = 'GET', body = null, headers = {}) => {
+    setLoading(true)
     try {
+      if (body) {
+        body = JSON.stringify(body)
+        headers['Content-Type'] = 'application/json'
+      }
+
       const response = await fetch(url, {method, body, headers})
       const data = await response.json()
 
       if (!response.ok) {
         throw new Error(data.message || 'Что-то пошло не так')
       }
-    } catch (e) {
 
+      setLoading(false)
+
+      return data
+    } catch (e) {
+      setLoading(false)
+      setError(e.message)
+      throw e
     }
   }, [])
 
-  return { loading, request, error }
+  const clearError = useCallback( () => setError(null), [])
+
+  return { loading, request, error, clearError }
 }
--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,15 +1,42 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useHttp } from '../hooks/http.hook'
+import { useMessage } from '../hooks/message.hook'
+import { AuthContext } from '../context/AuthContext'
 
 export const AuthPage = () => {
+  const auth = useContext(AuthContext)
+  const message = useMessage()
+  const { loading, request, error, clearError } = useHttp()
   const [form, setForm] = useState({
     email: '', password: ''
   })
+
+  useEffect(() => {
+    message(error)
+    clearError()
+  }, [error, message, clearError])
+
+  useEffect(() => {
+    window.M.updateTextFields()
+  }, [])
 
   const changeHandler = event => {
     setForm({ ...form, [event.target.name]: event.target.value })
   }
 
-  
+  const registerHandler = async () => {
+    try {
+      const data = await request('/api/auth/register', 'POST', { ...form })
+      message(data.message)
+    } catch (e) {}
+  }
+
+  const loginHandler = async () => {
+    try {
+      const data = await request('/api/auth/login', 'POST', { ...form })
+      auth.login(data.token, data.userId)
+    } catch (e) {}
+  }
 
   return (
     <div className="row">
@@ -47,12 +74,24 @@ export const AuthPage = () => {
             </div>
           </div>
           <div className="card-action">
-            <button className="btn yellow darken-4" style={{ marginRight: 10 }}>Войти</button>
-            <button className="btn grey lighten-1 black-text">Регистрация</button>
+            <button 
+              className="btn yellow darken-4" 
+              style={{ marginRight: 10 }}
+              disabled={loading}
+              onClick={loginHandler}
+            >
+              Войти
+            </button>
+            <button 
+              className="btn grey lighten-1 black-text"
+              onClick={registerHandler}
+              disabled={loading}
+            >
+              Регистрация
+            </button>
           </div>
         </div>
       </div>
     </div>
   )
 }
-
